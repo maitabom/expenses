535bfb3973f584df077a45524698e64eb6d8065b
Fábio Valentim <maitabom@gmail.com>
2025-05-11 19:13:51 -0300

    feat: substituir SingleChildScrollView por ListView.builder para melhor desempenho na lista de transações

--- lib/components/transaction_list.dart
+++ lib/components/transaction_list.dart
@@ -11,58 +11,51 @@ class TransactionList extends StatelessWidget {
   Widget build(BuildContext context) {
     return SizedBox(
       height: 400,
-      child: SingleChildScrollView(
-        child: Column(
-          children:
-              transactions.map((tr) {
-                return Card(
-                  child: Row(
-                    children: [
-                      Container(
-                        margin: EdgeInsets.symmetric(
-                          horizontal: 15,
-                          vertical: 10,
-                        ),
-                        padding: EdgeInsets.all(10),
-                        decoration: BoxDecoration(
-                          border: Border.all(
-                            color: Colors.purple.shade50,
-                            width: 2,
-                          ),
-                        ),
-                        child: Text(
-                          NumberFormat.currency(
-                            symbol: 'R\$',
-                            decimalDigits: 2,
-                          ).format(tr.value),
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 20,
-                            color: Colors.purple,
-                          ),
-                        ),
-                      ),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            tr.title,
-                            style: TextStyle(
-                              fontSize: 16,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                          Text(
-                            DateFormat('d MMM y').format(tr.date),
-                            style: TextStyle(color: Colors.grey.shade700),
-                          ),
-                        ],
-                      ),
-                    ],
+      child: ListView.builder(
+        itemCount: transactions.length,
+        itemBuilder: (context, index) {
+          final tr = transactions[index];
+          return Card(
+            child: Row(
+              children: [
+                Container(
+                  margin: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+                  padding: EdgeInsets.all(10),
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.purple.shade50, width: 2),
                   ),
-                );
-              }).toList(),
-        ),
+                  child: Text(
+                    NumberFormat.currency(
+                      symbol: 'R\$',
+                      decimalDigits: 2,
+                    ).format(tr.value),
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                      color: Colors.purple,
+                    ),
+                  ),
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      tr.title,
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Text(
+                      DateFormat('d MMM y').format(tr.date),
+                      style: TextStyle(color: Colors.grey.shade700),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          );
+        },
       ),
     );
   }
