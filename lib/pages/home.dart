59597c9ac1f24c4f911c8eb4f11484c3cc4897b1
Fábio Valentim <maitabom@gmail.com>
2025-05-17 17:02:29 -0300

    feat: reorganizar o código do AppBar para melhor legibilidade

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -7,12 +7,21 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Despesas Pessoais')),
+      appBar: AppBar(
+        title: Text('Despesas Pessoais'),
+        actions: [
+          IconButton(icon: Icon(Icons.add_circle_outline), onPressed: () {}),
+        ],
+      ),
       body: SingleChildScrollView(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[Card(child: Text('Gráfico')), TransactionUser()],
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {},
       ),
     );
   }
